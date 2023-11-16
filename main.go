6865268184d3ce38a6e9fbee4c3d9a4ad6e9a1dc
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-11-16 10:56:59 +0100

    add basic server

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("basic server is running!"))
+}	
